affe3b031cc9f80fec6447b6d13a6579652fd245
João Gondim <gondimjoaom@gmail.com>
2021-08-21 17:00:42 -0300

    removida transmissao rtsp

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import cv2
 import torch
 import torch.backends.cudnn as cudnn
 from numpy import random
-from utils import stream
 
 from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
@@ -14,7 +13,6 @@ from utils.general import check_img_size, check_requirements, check_imshow, non_
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
 from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
-from utils.stream import Streamer
 from flask import request, Response, Flask
 import threading
 
@@ -131,10 +129,6 @@ class Detector():
                 if view_img:
                     cv2.imshow(str(p), im0)
                     cv2.waitKey(1)  # 1 millisecond
-                if opt.rtsp:
-                    if streamer is None:
-                        streamer = Streamer(im0)
-                    streamer.sendFrame(im0)
                 self.currentFrame = im0
                 self.frameCount -=- 1
                 # Save results (image with detections)
